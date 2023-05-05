80b3d2a54f5776443d7663c344aa5c89608bf41a
Last <noreply@gitlab.lab.local>
2023-05-05 01:54:36 -0500

    Add catppuccin theme

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,18 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./app/**/*.{js,ts,jsx,tsx}", "./src/**/*.{js,ts,jsx,tsx}"],
+  content: [
+		"./pages/**/*.{js,ts,jsx,tsx}",
+    "./app/**/*.{js,ts,jsx,tsx}",
+		"./components/**/*.{js,ts,jsx,tsx}",
+	],
   theme: {
     extend: {},
   },
-  plugins: [],
+  plugins: [
+		require("@tailwindcss/forms"),
+		require("@catppuccin/tailwindcss")({
+			prefix: false,
+			defaultFlavour: "frappe",
+		}),
+  ],
 };
